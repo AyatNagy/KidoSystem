b7d981cf9ddb53d7f6bec2e6b226c4d177e62098
Habiba Nasser <habnasgomaa@gmail.com>
2025-10-12 14:22:11 -0700

    Update Start_Page.dart

--- lib/Pages/Start_Page.dart
+++ lib/Pages/Start_Page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:kido/Pages/onboard_page.dart';
-import 'package:kido/Widgets/GradientButton.dart';
+import 'package:kido/home_page.dart';
+import 'package:kido/unboarding_page.dart';
 
 class Start extends StatelessWidget {
   const Start({super.key});
@@ -15,52 +15,98 @@ class Start extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Align(
-              alignment: Alignment.topLeft,
-              child: Row(
+                alignment: Alignment.topLeft,
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'images/log.png',
+                      height: 50,
+                      width: 60,
+                    ),
+                    Image.asset(
+                      'images/kido.png',
+                      height: 60,
+                      width: 60,
+                    ),
+                  ],
+                )
+            ),
+            Center(
+              child:Expanded(
+                  child:Image.asset(
+                    'images/Start.png',
+                    height: 400,
+                    width: 400,
+                    fit: BoxFit.contain,
+                  )
+              )
+            ),
+              Column(
                 children: [
-                  Image.asset('assets/images/log.png', height: 50, width: 60),
-                  Image.asset('assets/images/Kido.png', height: 50, width: 60),
+                  const SizedBox(height: 1),
+                  const Text(
+                    "Every Child’s\nJourney to Their Star!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize:30,
+                      shadows: [
+                        Shadow(
+                          offset: Offset(2, 2),
+                          blurRadius: 3,
+                          color: Colors.grey,
+                        ),
+                      ],
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                      height: 1.3,
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 56,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => OnboardingScreen()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        padding: EdgeInsets.zero,
+                        elevation: 3,
+                        backgroundColor: Colors.transparent,
+                      ),
+                      child: Ink(
+                        decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xFFE68A5C),
+                              Color(0xFF8869B3),
+                              Color(0xFF4C99A8),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                        ),
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: const Text(
+                            "Start Exploring!",
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 50,),
                 ],
               ),
-            ),
-            Row(
-              children: [
-                Image.asset('assets/images/start1.png', height: 260),
-                Image.asset('assets/images/start2.jpeg', height: 260),
-                Image.asset('assets/images/start3.jpeg', height: 260),
-              ],
-            ),
-            Column(
-              children: [
-                const SizedBox(height: 16),
-                const Text(
-                  "Every Child’s\nJourney to Their Star!",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(3, 3),
-                        blurRadius: 3,
-                        color: Colors.grey,
-                      ),
-                    ],
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black87,
-                    height: 1.3,
-                  ),
-                ),
-                const SizedBox(height: 28),
-                GradientButton(
-                  title: "Start Exploring!",
-                  onPressed: () {
-                  Navigator.pushReplacement( context, 
-                  MaterialPageRoute(builder: (context) => OnboardScreen()),
-                   );
-                  },
-                ),
-              ],
-            ),
           ],
         ),
       ),
