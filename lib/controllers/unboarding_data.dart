6df54ab638774817fd36e57f0d16920c1f9226e7
Habiba Nasser <habnasgomaa@gmail.com>
2025-10-31 13:24:50 -0700

    Update unboarding_data.dart

--- lib/controllers/unboarding_data.dart
+++ lib/controllers/unboarding_data.dart
@@ -2,22 +2,22 @@ import 'package:kido/Models/OnboardModel.dart';
 
 final List<OnboardModel> onboardData = [
   OnboardModel(
-    image: 'images/learn.jpeg',
+    image: 'assets/images/learn.jpeg',
     title: 'Learn',
     desc: 'Discover fun lessons with letters, numbers, shapes, and more!',
   ),
   OnboardModel(
-    image: 'images/play.jpeg',
+    image: 'assets/images/play.jpeg',
     title: 'Play',
     desc: 'Enjoy interactive games and quizzes that make learning exciting.',
   ),
   OnboardModel(
-    image: 'images/Connect.png',
+    image: 'assets/images/Connect.png',
     title: 'Connect',
     desc: 'Track your child’s progress and stay in touch with teachers.',
   ),
   OnboardModel(
-    image: 'images/guide.jpeg',
+    image: 'assets/images/guide.jpeg',
     title: 'Guide',
     desc: 'Assess levels, assign activities, and create extra exams when needed.',
   ),
