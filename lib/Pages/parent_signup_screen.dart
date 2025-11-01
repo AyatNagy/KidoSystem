241b5434f1e6ae0080814f2f084376a217d0a8cf
Habiba Nasser <habnasgomaa@gmail.com>
2025-11-01 12:29:29 -0700

    Update onboard_page.dart

--- lib/Pages/parent_signup_screen.dart
+++ lib/Pages/parent_signup_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kido/Pages/student_data_screen.dart';
 import '../Widgets/text_field_item.dart';
 import '../utils/validators.dart';
 import 'parent_login_screen.dart';
@@ -16,32 +17,32 @@ class ParentSignup extends StatefulWidget {
 class _ParentSignupState extends State<ParentSignup> {
 
   final TextEditingController emailController = TextEditingController();
-   final TextEditingController passwordController = TextEditingController();
-   final TextEditingController nameController = TextEditingController();
-   final TextEditingController phoneController = TextEditingController();
-   final phoneRegex=RegExp(r'^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$');
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final phoneRegex=RegExp(r'^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$');
 
-   bool isPasswordVisible=false;
-   final _formKey = GlobalKey<FormState>();
+  bool isPasswordVisible=false;
+  final _formKey = GlobalKey<FormState>();
 
-    void handle(){
+  void handle(){
     if(_formKey.currentState!.validate()){
       print("success");
     }
-    }
+  }
 
 
   @override
   Widget build(BuildContext context) {
     return
 
-       Scaffold(
+      Scaffold(
         body: SafeArea(
           child: SingleChildScrollView(
-             padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
             child: Column(
-               mainAxisAlignment: MainAxisAlignment.center,
-               //crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.center,
 
               children: [
                 //const SizedBox(height: 20,),
@@ -50,155 +51,160 @@ class _ParentSignupState extends State<ParentSignup> {
                   children: [
                     Image.asset('assets/images/log.png',height: 40,),
                     const SizedBox(height: 6,),
-                    Image.asset('assets/images/Kido.png',height: 40,)
+                    //Image.asset('assets/images/Kido.png',height: 40,)
                   ],
                 ),
 
                 const SizedBox(height: 20,),
 
                 Text(
-                     "Hi,Parent!",
+                    "Hi,Parent!",
                     style:TextStyle(
                       fontSize: 42,
                       fontWeight: FontWeight.bold,
                       shadows: [
-                    Shadow(
-                      blurRadius: 2,
-                      offset: Offset(0.5, 0.5),
-                      color: Colors.black26,
-                    )
-                  ],
+                        Shadow(
+                          blurRadius: 2,
+                          offset: Offset(0.5, 0.5),
+                          color: Colors.black26,
+                        )
+                      ],
 
-                )),
+                    )),
 
                 const SizedBox(height:10,),
 
                 Image.asset(
                   'assets/images/parent_sign up.png',
-                   height: 336,width: 339,),
+                  height: 336,width: 339,),
 
 
-              SizedBox(height: 20,),
+                SizedBox(height: 20,),
 
-               Form(
-                key: _formKey,
-                 child: Column(
-                  children: [
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
 
-                     CustomTextField(
-                    fieldController: nameController ,
-                    fieldIcon: Icon(Icons.person),
-                    fieldLabel: "Full Name",
-                    fieldObscure: false,
-                    validator:Validators.validateName,
+                      CustomTextField(
+                        fieldController: nameController ,
+                        fieldIcon: Icon(Icons.person),
+                        fieldLabel: "Full Name",
+                        fieldObscure: false,
+                        validator:Validators.validateName,
 
-                  ),
-
-                   const SizedBox(height: 16,),
-
-                   CustomTextField(
-                    fieldController: phoneController ,
-                    fieldIcon: Icon(Icons.phone),
-                    fieldLabel: "Phone Number",
-                    fieldObscure: false,
-                    keyboardType: TextInputType.number,
-                    validator: (value){
-                      if(value == null || value.isEmpty){
-                        return "Please enter your phone!";
-                      }
-
-                       if(!phoneRegex.hasMatch(value)){
-                        return "Please enter a valid phone number";
-                      }
-
-                      return null;
-                    },
-
-                  ),
-
-                   const SizedBox(height: 16,),
-
-
-                     CustomTextField(
-                    fieldController: emailController ,
-                    fieldIcon: Icon(Icons.email),
-                    fieldLabel: "Email",
-                    fieldObscure: false,
-                    validator: Validators.validateEmail,
-
-                  ),
-
-                  const SizedBox(height: 16,),
-
-                  CustomTextField(
-                    fieldController: passwordController ,
-                    fieldIcon: Icon(Icons.lock),
-                    fieldLabel: "Password",
-                    fieldObscure: !isPasswordVisible,
-                    suffixIcon: IconButton(
-                      onPressed:
-                      (){
-                        setState(() {
-                           isPasswordVisible=!isPasswordVisible;
-                        });
-                      },
-                      icon: Icon(
-                         isPasswordVisible ? Icons.visibility : Icons.visibility_off , color: Color(0xff837F7F)
-                      )),
-                    validator: Validators.validatePassword,
-
-                  ),
-
-                  const SizedBox(height: 30,),
-
-                  Container(
-                    width: 200,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(colors: [Color(0xffF8AA3B),Color(0xffFF7A78),Color(0xffEE3187)],
-                                               begin: Alignment.centerLeft,
-                                               end:Alignment.centerRight),
-                      borderRadius: BorderRadius.circular(25),
-
-                    ),
-                    child: ElevatedButton(
-                      onPressed: handle,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                        shadowColor: Colors.transparent,
-                          padding: const EdgeInsets.symmetric(vertical: 14),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25),
-                          ),
                       ),
-                      child: Text("Create Account",
-                      style: TextStyle(fontSize: 22,color: Colors.white,fontWeight: FontWeight.bold),)),
 
+                      const SizedBox(height: 16,),
 
-                  ),
+                      CustomTextField(
+                        fieldController: phoneController ,
+                        fieldIcon: Icon(Icons.phone),
+                        fieldLabel: "Phone Number",
+                        fieldObscure: false,
+                        keyboardType: TextInputType.number,
+                        validator: (value){
+                          if(value == null || value.isEmpty){
+                            return "Please enter your phone!";
+                          }
 
-                   const SizedBox(height: 16,),
+                          if(!phoneRegex.hasMatch(value)){
+                            return "Please enter a valid phone number";
+                          }
 
-
-
-                   // const SizedBox(height: 8,),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("Have an account?",
-                        style: TextStyle(color: Color(0xff837F7F))),
-
-                        TextButton(onPressed: (){
-                          Navigator.push(context,MaterialPageRoute(builder: (context)=>ParentLogin()));
+                          return null;
                         },
-                         child: Text("Log In",
-                         style: TextStyle(color: Color(0xffEE3187),fontWeight: FontWeight.bold)))
 
-                      ],
-                    )
-                  ],
-                 ),
-               )
+                      ),
+
+                      const SizedBox(height: 16,),
+
+
+                      CustomTextField(
+                        fieldController: emailController ,
+                        fieldIcon: Icon(Icons.email),
+                        fieldLabel: "Email",
+                        fieldObscure: false,
+                        validator: Validators.validateEmail,
+
+                      ),
+
+                      const SizedBox(height: 16,),
+
+                      CustomTextField(
+                        fieldController: passwordController ,
+                        fieldIcon: Icon(Icons.lock),
+                        fieldLabel: "Password",
+                        fieldObscure: !isPasswordVisible,
+                        suffixIcon: IconButton(
+                            onPressed:
+                                (){
+                              setState(() {
+                                isPasswordVisible=!isPasswordVisible;
+                              });
+                            },
+                            icon: Icon(
+                                isPasswordVisible ? Icons.visibility : Icons.visibility_off , color: Color(0xff837F7F)
+                            )),
+                        validator: Validators.validatePassword,
+
+                      ),
+
+                      const SizedBox(height: 30,),
+
+                      Container(
+                        width: 200,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(colors: [Color(0xffF8AA3B),Color(0xffFF7A78),Color(0xffEE3187)],
+                              begin: Alignment.centerLeft,
+                              end:Alignment.centerRight),
+                          borderRadius: BorderRadius.circular(25),
+
+                        ),
+                        child: ElevatedButton(
+                            onPressed:(){
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => StudentData()),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                              shadowColor: Colors.transparent,
+                              padding: const EdgeInsets.symmetric(vertical: 14),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                            ),
+                            child: Text("Create Account",
+                              style: TextStyle(fontSize: 22,color: Colors.white,fontWeight: FontWeight.bold),)),
+
+
+                      ),
+
+                      const SizedBox(height: 16,),
+
+
+
+                      // const SizedBox(height: 8,),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Have an account?",
+                              style: TextStyle(color: Color(0xff837F7F))),
+
+                          TextButton(onPressed: (){
+                            Navigator.push(context,MaterialPageRoute(builder: (context)=>ParentLogin()));
+                          },
+                              child: Text("Log In",
+                                  style: TextStyle(color: Color(0xffEE3187),fontWeight: FontWeight.bold)))
+
+                        ],
+                      )
+                    ],
+                  ),
+                )
 
 
 
@@ -212,4 +218,4 @@ class _ParentSignupState extends State<ParentSignup> {
       );
 
   }
-    }
+}
