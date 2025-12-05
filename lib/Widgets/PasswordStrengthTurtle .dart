8d7f452c0bd7945cbb1b344ae7d4bf6a1164d797
AyatNagy <ayatn4580@gmail.com>
2025-12-05 19:13:53 +0200

    forget pass logic

---
+++ lib/Widgets/PasswordStrengthTurtle .dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import '../utils/validators.dart';
+
+class PasswordStrengthTurtle extends StatefulWidget {
+  final String password;
+
+  const PasswordStrengthTurtle({super.key, required this.password});
+
+  @override
+  State<PasswordStrengthTurtle> createState() => _PasswordStrengthTurtleState();
+}
+
+class _PasswordStrengthTurtleState extends State<PasswordStrengthTurtle>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+      lowerBound: 0.0,
+      upperBound: 1.0,
+    );
+    final strength = Validators.getPasswordStrength(widget.password);
+    _controller.value = strength / 4.0;
+  }
+
+  @override
+  void didUpdateWidget(covariant PasswordStrengthTurtle oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    final strength = Validators.getPasswordStrength(widget.password);
+    double target;
+    target = strength / 4.0;
+    _controller.animateTo(
+      target,
+      duration: const Duration(milliseconds: 800),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  Color getStrengthColor(double strengthValue) {
+    if (strengthValue <= 0.25) return Colors.red;
+    if (strengthValue <= 0.5) return Colors.orange;
+    return Colors.green;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final int currentStrength = Validators.getPasswordStrength(widget.password);
+
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        final double maxMovementWidth = MediaQuery.of(context).size.width - 80;
+        final double turtleOffset = _controller.value * maxMovementWidth;
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 50,
+              child: Transform.translate(
+                offset: Offset(turtleOffset, 0),
+                child: Lottie.asset(
+                  'assets/lottie/turtle.json',
+                  controller: _controller,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+            LinearProgressIndicator(
+              value: _controller.value,
+              color: getStrengthColor(currentStrength / 4.0),
+              backgroundColor: Colors.grey.shade300,
+              minHeight: 5,
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
--- lib/utils/validators.dart
+++ lib/utils/validators.dart
@@ -1,44 +1,54 @@
-class Validators{
+class Validators {
+  static final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+  static final passwordRegex = RegExp(
+    r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$',
+  );
 
- static final emailRegex=RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
- static final passwordRegex=RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$');
+  static bool isValid(String password) {
+    return passwordRegex.hasMatch(password);
+  }
+
+  static int getPasswordStrength(String password) {
+    int c = 0;
+
+    if (password.length >= 8) c++;
+    if (RegExp(r'[A-Z]').hasMatch(password)) c++;
+    if (RegExp(r'[0-9]').hasMatch(password)) c++;
+    if (RegExp(r'[!@#\$%^&*(),.?":{}|<>]').hasMatch(password)) c++;
+
+    return c;
+  }
+
   //final phoneRegex=RegExp(r'^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$');
 
+  static String? validateEmail(String? email) {
+    if (email == null || email.isEmpty) {
+      return "Please enter your email!";
+    }
 
+    if (!emailRegex.hasMatch(email)) {
+      return "Please enter a valid email";
+    }
 
- static String? validateEmail(String? email){
-     if(email == null || email.isEmpty){
-                        return "Please enter your email!";
-                      }
-            
-                       if(!emailRegex.hasMatch(email)){
-                        return "Please enter a valid email";
-                      }
-            
-                      return null;
+    return null;
   }
 
+  static String? validatePassword(String? password) {
+    if (password == null || password.isEmpty) {
+      return "Please enter your password!";
+    }
 
-  static String? validatePassword(String? password){
-    if(password == null || password.isEmpty){
-                        return "Please enter your password!";
-                      }
-            
-                       if(!passwordRegex.hasMatch(password)){
-                        return "Please enter a valid password";
-                      }
-                      return null;
+    if (!passwordRegex.hasMatch(password)) {
+      return "Please enter a valid password";
+    }
+    return null;
   }
 
+  static String? validateName(String? name) {
+    if (name == null || name.isEmpty) {
+      return "Please enter your name!";
+    }
 
-  static String? validateName(String? name){
-     if(name == null || name.isEmpty){
-                        return "Please enter your name!";
-                      }
-            
-                       
-            
-                      return null;
+    return null;
   }
-
 }
