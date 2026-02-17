8ccb91f3ba1f37157b506528bdf96594114706db
Habiba Nasser <habnasgomaa@gmail.com>
2026-02-16 22:26:40 -0800

    Feelings

---
+++ lib/controllers/feelings_data.dart
@@ -0,0 +1,27 @@
+List<Map<String, String>> activeChoices = [
+  {
+    'id': '1',
+    'action': 'assets/images/gift.png',
+    'reaction': 'assets/images/happy_boy.png'
+  },
+  {
+    'id': '2',
+    'action': 'assets/images/medal.png',
+    'reaction': 'assets/images/proudBoy.png'
+  },
+  {
+    'id': '3',
+    'action': 'assets/images/ghost.png',
+    'reaction': 'assets/images/scared_boy.png'
+  },
+  {
+    'id': '4',
+    'action': 'assets/images/broken_heart.png',
+    'reaction': 'assets/images/sad_boy.png'
+  },
+  {
+    'id': '5',
+    'action': 'assets/images/popped_ballon.png',
+    'reaction': 'assets/images/angry_boy.png'
+  },
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'Pages/Logo_Page.dart';
+import 'Pages/draganddrop_feelings.dart';
 import 'Widgets/info_widget.dart';
 import 'Widgets/ResponsiveProvider.dart';
 import 'config/ResponsiveConfig.dart';
