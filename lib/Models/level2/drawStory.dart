dde45c246936fa6ed953781c63633e36001cf975
Habiba Nasser <habnasgomaa@gmail.com>
2026-03-13 20:37:50 +0200

    level2

---
+++ lib/Models/level2/drawStory.dart
@@ -0,0 +1,13 @@
+class LessonFrame {
+  final String imagePath;
+
+  LessonFrame({required this.imagePath,});
+}
+
+final List<LessonFrame> drawingStory = [
+  LessonFrame(imagePath: 'assets/images/start-draw.png'),
+  LessonFrame(imagePath: 'assets/images/pen-draw.png'),
+  LessonFrame(imagePath: 'assets/images/part-draw.png'),
+  LessonFrame(imagePath: 'assets/images/finish1-draw.png'),
+  LessonFrame(imagePath: 'assets/images/finish2-draw.png'),
+];
