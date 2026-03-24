60cb6a18126c1e506f3d37060ba214ab5232372f
Habiba Nasser <habnasgomaa@gmail.com>
2026-03-24 20:26:55 +0200

    map

--- lib/Widgets/map.dart
+++ lib/Widgets/map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // For HapticFeedback
 import 'dart:math' as math;
 import '../Models/mapModel.dart';
 import 'ResponsiveProvider.dart';
@@ -23,23 +24,39 @@ class MapNode extends StatefulWidget {
   State<MapNode> createState() => _MapNodeState();
 }
 
-class _MapNodeState extends State<MapNode> {
+class _MapNodeState extends State<MapNode> with SingleTickerProviderStateMixin {
   bool _isPressed = false;
+  late AnimationController _pulseController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pulseController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _pulseController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final config = ResponsiveProvider.of(context);
     final double screenWidth = config.localWidth;
 
-    const double nodeSize = 90.0;
-    const double vPadding = 45.0;
+    const double nodeSize = 100.0;
+    const double vPadding = 50.0;
+
     const Color pathColor = Color(0xFFFFF4D1);
-    const Color activeColor = Color(0xFF58CC02);
     const Color lockedColor = Color(0xFFD4B483);
 
     double calcX(int i) {
-      double wave = math.sin(i * 0.8);
-      return (screenWidth / 2) + (wave * (screenWidth / 4)) - (nodeSize / 2);
+      double wave = math.sin(i * 0.7);
+      return (screenWidth / 2) + (wave * (screenWidth / 5)) - (nodeSize / 2);
     }
 
     double xOffset = calcX(widget.index);
@@ -71,36 +88,45 @@ class _MapNodeState extends State<MapNode> {
               alignment: Alignment.centerLeft,
               child: Padding(
                 padding: EdgeInsets.only(left: xOffset),
-                child: GestureDetector(
-                  onTapDown: (_) => setState(() => _isPressed = true),
-                  onTapUp: (_) => setState(() => _isPressed = false),
-                  onTapCancel: () => setState(() => _isPressed = false),
-                  onTap: widget.lesson.isLocked ? null : widget.onTap,
-                  child: AnimatedContainer(
-                    duration: const Duration(milliseconds: 100),
-                    height: nodeSize,
-                    width: nodeSize,
-                    transform: Matrix4.translationValues(0, _isPressed ? 8 : 0, 0),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: widget.lesson.isLocked ? lockedColor : widget.buttonColor,
-                      border: Border.all(
-                        color: _isPressed ? Colors.yellowAccent : Colors.white.withOpacity(0.8),
-                        width: 6,
-                      ),
-                      boxShadow: [
-                        if (!_isPressed)
+                child: ScaleTransition(
+                  scale: widget.lesson.isLocked
+                      ? const AlwaysStoppedAnimation(1.0)
+                      : Tween(begin: 1.0, end: 1.05).animate(_pulseController),
+                  child: GestureDetector(
+                    onTapDown: (_) {
+                      HapticFeedback.mediumImpact();
+                      setState(() => _isPressed = true);
+                    },
+                    onTapUp: (_) => setState(() => _isPressed = false),
+                    onTapCancel: () => setState(() => _isPressed = false),
+                    onTap: widget.lesson.isLocked ? null : widget.onTap,
+                    child: AnimatedContainer(
+                      duration: const Duration(milliseconds: 100),
+                      height: nodeSize,
+                      width: nodeSize,
+                      transform: Matrix4.translationValues(0, _isPressed ? 6 : 0, 0),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: widget.lesson.isLocked ? lockedColor : widget.buttonColor,
+                        border: Border.all(
+                          color: _isPressed ? Colors.white : Colors.black.withOpacity(0.2),
+                          width: 8,
+                        ),
+                        boxShadow: [
                           BoxShadow(
-                            color: Colors.black.withOpacity(0.15),
-                            offset: const Offset(0, 10),
+                            color: widget.lesson.isLocked
+                                ? Colors.grey.shade600
+                                : widget.buttonColor.withAlpha(150),
+                            offset: Offset(0, _isPressed ? 2 : 12),
                             blurRadius: 0,
                           ),
-                      ],
-                    ),
-                    child: Center(
-                      child: widget.lesson.isLocked
-                          ? const Icon(Icons.lock_outline, color: Colors.white70, size: 35)
-                          : Image.asset(widget.lesson.image, width: 50),
+                        ],
+                      ),
+                      child: Center(
+                        child: widget.lesson.isLocked
+                            ? const Icon(Icons.lock, color: Colors.white, size: 45)
+                            : Image.asset(widget.lesson.image, width: 60),
+                      ),
                     ),
                   ),
                 ),
