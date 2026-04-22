7fd7ead4d07630c272580b62d9dab29a3e9d0896
Arwa--Bahaa11 <arwabahaa27@gmail.com>
2026-04-22 21:06:07 +0200

    edit on numbers lesson

---
+++ lib/Widgets/sound_button.dart
@@ -0,0 +1,47 @@
+import'package:flutter/material.dart';
+
+class SoundButton extends StatelessWidget{
+
+  final Color color;
+  final Color shadowColor;
+  final VoidCallback onPressed;
+
+  const SoundButton({
+    super.key,
+    required this.color,
+    required this.onPressed,
+    this.shadowColor= Colors.amber,
+
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+          boxShadow: [(
+            BoxShadow(
+              color:shadowColor.withOpacity(0.6),
+              blurRadius: 15,
+              spreadRadius: 5,
+              offset: const Offset(0, 0)
+            )
+            
+          )
+          ]
+        ),
+
+        child: const Icon(
+          Icons.volume_up_rounded,
+          size: 35,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+}
