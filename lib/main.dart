a6addc4bd26900d6564ccdd67258e0262059dc31
sarayounan <sarahfouad64@gmail.com>
2026-05-03 16:49:32 +0300

    solve conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:device_preview/device_preview.dart';
-<<<<<<< HEAD
-import 'package:kido/Pages/content/sizes/size_intro_page.dart';
-import 'package:kido/Pages/level1/matching_practice_page.dart';
-=======
 import 'package:audioplayers/audioplayers.dart';
 import 'package:kido/Pages/level3/family_members/FamilyBackGround.dart';
->>>>>>> main
 import 'package:kido/Pages/level3/level3_home.dart';
 import 'package:kido/Widgets/responsive_provider.dart';
 import 'package:kido/config/responsive_config.dart';
@@ -32,13 +27,7 @@ class MyApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             builder: DevicePreview.appBuilder,
             locale: DevicePreview.locale(context),
-<<<<<<< HEAD
-            home: const MatchingPracticePage(),
-
-            //SenseTapPracticeScreen(type: SenseType.ears),
-=======
             home: const FamilyBackGround(),
->>>>>>> main
           ),
         );
       },
